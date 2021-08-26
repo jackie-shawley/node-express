d276e9407b23eda15f21040d13df01ee98d7deb9
jackie-shawley <jackiekopco@aol.com>
2021-08-26 18:20:59 -0400

    Express Simple REST

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();  //returns an express server application available by using the variable app
-
 app.use(morgan('dev'));  //use 'dev' to get more info printed to the console
+app.use(express.json()); //when the server receives request in JSON format in the body, this middlewear will parse that data into JavaScript properties of the request object so that we can use that data in JS 
+
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;  //this is setting the statusCode for all of the requests, so no need to put them in the upcoming requests
+    res.setHeader('Content-Type', 'text/plain'); //this is setting the header for all of the requests, so no need to put them in the upcoming requests
+    next(); //will pass control of the application routing to the next relevent routing method after this one. Otherwise it would stop here.
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;  //403 is the error code when the operation is not supported
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403; 
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+}); //we can use req.body.xx because we have parsed them out from the data that was sent to us; it is being echoed back as text
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 app.use(express.static(__dirname + '/public'));
 
